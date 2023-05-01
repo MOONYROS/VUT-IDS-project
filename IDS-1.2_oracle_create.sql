7001b0a55e8b75acf7e5ff6d9bb0431e0ab01cdc
Ondrej Lukasek <ondrej@lukasek.cz>
2023-05-01 18:05:41 +0200

    Are we done? PLS!? ARE WE DONE!????

--- IDS-1.2_oracle_create.sql
+++ IDS-1.2_oracle_create.sql
@@ -332,48 +332,52 @@ VALUES(auto_num.currval, 'hotove');
 -- DOTAZY SELECT (3. ODEVZDANI)
 
 -- Vybira, za kolik penez se v objednavkach koupilo dane zbozi (Spojeni 2 tabulek).
--- SELECT ID_ob_zb, cena
--- FROM Objednavka INNER JOIN Faktura ON Faktura.ID_fak = Objednavka.ID_obj;
---
--- -- Vybira, kolik utratil zakaznik se jmenem Pan Zakaznik (Spojeni 2 tabulek).
--- SELECT jmeno, prijmeni, utrata
--- FROM Osoba INNER JOIN Zakaznik ON Osoba.ID_osoby = Zakaznik.ID_zak
--- WHERE jmeno = 'Pan' AND prijmeni = 'Zakaznik';
---
--- -- Vybira, prijmeni zamestancu, kteri pracuji na ktere pokladne a zaroven jejichz pracovni pomer je DPP (Spojeni 3 tabulek).
--- SELECT prijmeni, ID_pokl
--- FROM Osoba INNER JOIN Zames ON Osoba.ID_osoby = Zames.ID_zam INNER JOIN Pokladna ON Osoba.ID_osoby = Pokladna.ID_oso
--- WHERE prac_pom = 'DPP';
---
--- -- Vybira pocet objednavek podle typu zbozi s cenou vyssi nez 10.000 (GROUP BY + AGREGACNI FUNKCE).
--- SELECT typ, count(*) pocet_obj
--- FROM Zbozi INNER JOIN Objednavka ON Zbozi.ID_zbozi = Objednavka.ID_ob_zb
--- WHERE cena >= 10000
--- GROUP BY typ;
---
--- -- Vybira, kolik utratili zakaznici co kartu maji nebo nemaji (GROUP BY + AGREGACNI FUNKCE).
--- SELECT zak_karta, sum(utrata)
--- FROM Zakaznik
--- GROUP BY zak_karta;
---
--- -- Vybira zamestance, co maji zadany jak email, tak telefonni cislo (POUZITI OPERATORU EXISTS).
--- SELECT jmeno, prijmeni
--- FROM Osoba
--- WHERE EXISTS(SELECT * FROM Zames WHERE Osoba.ID_osoby = Zames.ID_zam AND telefon IS NOT NULL AND email IS NOT NULL);
---
--- -- Vybira, jaky typ zbozi dodavaji dodavatele, co jsou spolecnost s rucenim omezenym (POUZITI OPERATORU IN).
--- SELECT dodav, typ
--- FROM Zbozi
--- WHERE ID_zbozi IN(SELECT ID_zbozi FROM Zbozi WHERE dodav LIKE '%s.r.o');
+SELECT ID_ob_zb, cena
+FROM Objednavka INNER JOIN Faktura ON Faktura.ID_fak = Objednavka.ID_obj;
+
+-- Vybira, kolik utratil zakaznik se jmenem Pan Zakaznik (Spojeni 2 tabulek).
+SELECT jmeno, prijmeni, utrata
+FROM Osoba INNER JOIN Zakaznik ON Osoba.ID_osoby = Zakaznik.ID_zak
+WHERE jmeno = 'Pan' AND prijmeni = 'Zakaznik';
+
+-- Vybira, prijmeni zamestancu, kteri pracuji na ktere pokladne a zaroven jejichz pracovni pomer je DPP (Spojeni 3 tabulek).
+SELECT prijmeni, ID_pokl
+FROM Osoba INNER JOIN Zames ON Osoba.ID_osoby = Zames.ID_zam INNER JOIN Pokladna ON Osoba.ID_osoby = Pokladna.ID_oso
+WHERE prac_pom = 'DPP';
+
+-- Vybira pocet objednavek podle typu zbozi s cenou vyssi nez 10.000 (GROUP BY + AGREGACNI FUNKCE).
+SELECT typ, count(*) pocet_obj
+FROM Zbozi INNER JOIN Objednavka ON Zbozi.ID_zbozi = Objednavka.ID_ob_zb
+WHERE cena >= 10000
+GROUP BY typ;
+
+-- Vybira, kolik utratili zakaznici co kartu maji nebo nemaji (GROUP BY + AGREGACNI FUNKCE).
+SELECT zak_karta, sum(utrata)
+FROM Zakaznik
+GROUP BY zak_karta;
+
+-- Vybira zamestance, co maji zadany jak email, tak telefonni cislo (POUZITI OPERATORU EXISTS).
+SELECT jmeno, prijmeni
+FROM Osoba
+WHERE EXISTS(SELECT * FROM Zames WHERE Osoba.ID_osoby = Zames.ID_zam AND telefon IS NOT NULL AND email IS NOT NULL);
+
+-- Vybira, jaky typ zbozi dodavaji dodavatele, co jsou spolecnost s rucenim omezenym (POUZITI OPERATORU IN).
+SELECT dodav, typ
+FROM Zbozi
+WHERE ID_zbozi IN(SELECT ID_zbozi FROM Zbozi WHERE dodav LIKE '%s.r.o');
 
 -- 4. ODEVZDANI
 -- TRIGGER 1
+-- Napise do tabulky Nepovolena_objednavka_log, ze nekdo jiny nez reditel udelal objednavku za vice nez 50.000 Kc.
+-- Tu muze udelat pouze reditel, takze by ji mel schvalit.
 CREATE TABLE Nepovolena_objednavka_log (
     ID_ob_oso NUMBER(10),
     zprava VARCHAR2(50),
     cena FLOAT(20)
 );
 
+-- reditel ma ID 2341
+-- protoze mame jenom jednoho reditele, nemusime omezovat pozici, staci ID
 CREATE OR REPLACE TRIGGER Povolena_objednavka
     AFTER INSERT ON Objednavka
     FOR EACH ROW
@@ -393,29 +397,39 @@ EXCEPTION
 END;
 /
 
+-- Inserty pro predvedeni funkcnosti TRIGGERU.
+-- Trigger se ozve
 INSERT INTO Objednavka
 VALUES(123456, 1007, 3386, 60000);
 INSERT INTO Faktura
 VALUES(123456, 'kartou');
 
+-- Trigger se neozve
+-- Objednavku dela reditel
 INSERT INTO Objednavka
 VALUES(123457, 1007, 2341, 80000);
 INSERT INTO Faktura
 VALUES(123457, 'kartou');
 
+-- Trigger se neozve
+-- Neprekracuje mez
 INSERT INTO Objednavka
 VALUES(123458, 1005, 3386, 10000);
 INSERT INTO Faktura
 VALUES(123458, 'kartou');
 
+-- Trigger se ozve
 INSERT INTO Objednavka
 VALUES(123459, 1003, 2361, 100000);
 INSERT INTO Faktura
 VALUES(123459, 'kartou');
 
+-- Vypis logu
 SELECT * FROM Nepovolena_objednavka_log;
 
 -- TRIGGER 2
+-- Upozornuje, ze nejaky ze zamestnancu nema uveden zadny kontaktni udaj (ani email, ani telefon).
+-- Do malo_kontaktu_log se ulozi zaznam s ID zamestnance bez kontaktnich udaju.
 CREATE TABLE malo_kontaktu_log (
     ID_osoby NUMBER(10),
     zprava VARCHAR2(70)
@@ -446,15 +460,28 @@ EXCEPTION
 END;
 /
 
-
+-- INSERT na predvedeni funkcnosti TRIGGERU.
+-- Zamestnanec nema zadany ani telefon ani email.
 INSERT INTO Osoba
 VALUES (2362, 'Miluju', 'Triggery', '', '999999/9999');
 INSERT INTO Zames
 VALUES (8, 2362, 'skladnik', 38000, '', 'HPP');
 
+INSERT INTO Osoba
+VALUES (2363, 'Pani', 'Pokladni', 'tisknu@uctenky.cz', '888888/8888');
+INSERT INTO Zames
+VALUES (9, 2363, 'pokladni', 21000, '', 'HPP');
+
+INSERT INTO Osoba
+VALUES (2364, 'Leo', 'Galante', 'leo@empirebay.com', '870627/1887');
+INSERT INTO Zames
+VALUES (10, 2364, 'consigliere', 89000, 223082010, 'HPP');
+
+-- Vypis zaznamu z logu.
 SELECT * FROM malo_kontaktu_log;
 
 -- PROCEDURA 1
+-- Bere 2 parametry (pozici [VARCHAR2], plat [NUMBER]) a vsem zamestancum se zadanou pozici zmeni plat o zadany obnos.
 CREATE OR REPLACE PROCEDURE ZmenaPlatuPozice (
 	pozice IN VARCHAR2,
 	plat IN NUMBER
@@ -471,27 +498,40 @@ BEGIN
 	COMMIT;
 END;
 
-SELECT ID_zam, pozice, plat FROM Zames WHERE pozice = 'reditel'
-										  OR pozice = 'pokladni'
-										  OR pozice = 'vedouci';
+-- Ukazka pred zmenou platu
+SELECT ID_zam, pozice, plat
+FROM Zames
+WHERE pozice = 'reditel'
+   OR pozice = 'pokladni'
+   OR pozice = 'vedouci';
 
+-- Zmena platu reditele
 BEGIN
 	ZmenaPlatuPozice('reditel', 5001);
 END;
 
+-- Zmena platu pokladnich
 BEGIN
 	ZmenaPlatuPozice('pokladni', 1002);
 END;
 
+-- Zmena platu vedoucich
 BEGIN
 	ZmenaPlatuPozice('vedouci', -999);
 END;
+-- POZNAMKA: Muzeme i snizit plat zadanim zaporneho cisla
 
-SELECT ID_zam, pozice, plat FROM Zames WHERE pozice = 'reditel'
-										  OR pozice = 'pokladni'
-										  OR pozice = 'vedouci';
+-- Ukazka po zmene platu pozic
+SELECT ID_zam, pozice, plat
+FROM Zames
+WHERE pozice = 'reditel'
+   OR pozice = 'pokladni'
+   OR pozice = 'vedouci';
 
--- TODO PROCEDURA 2 (+ kurzor)
+-- PROCEDURA 2 (+ kurzor)
+-- Bere 2 parametry (stare jmeno [VARCHAR2], nove jmeno [VARCHAR2]).
+-- Zmeni jmeno zadaneho dodavatele na zadane nove jmeno.
+-- Vyuziva kurzor k nalezeni jmena, ktere je potreba zmenit.
 CREATE OR REPLACE PROCEDURE zmen_dodav(
     p_old_dodav VARCHAR2,
     p_new_dodav VARCHAR2
@@ -524,12 +564,15 @@ BEGIN
 END;
 /
 
+-- Ukazka pred zmenou jmena dodavatele
 SELECT dodav FROM Zbozi;
 
+-- Zmenime jmeno "Rumovar s.r.o" na "Rumovarna u modreho stromu s.r.o"
 BEGIN
     zmen_dodav('Rumovar s.r.o', 'Rumovarna u modreho stromu s.r.o');
 end;
 
+-- Ukazka po zmene jmena dodavatele
 SELECT dodav FROM Zbozi;
 
 
@@ -541,22 +584,30 @@ SELECT zpus_plat, sum(cena)
 FROM Objednavka INNER JOIN Faktura ON ID_obj = ID_fak
 GROUP BY zpus_plat;
 
+-- Vypsani planu pred zrychlenim
 EXPLAIN PLAN FOR
 	SELECT zpus_plat, sum(cena)
 	FROM Objednavka INNER JOIN Faktura ON ID_obj = ID_fak
 	GROUP BY zpus_plat;
 SELECT *
 FROM TABLE(DBMS_XPLAN.DISPLAY);
+-- Lze videt, ze bez zrychleni se projde 22 radky s cenou 4 na CPU a kompletnim pruchodem tabulkou Faktura.
 
-
+-- Vytvorime si index pro Fakturu, ze ktere vezmeme pouze ID a zpusob platby, dodavatel nas nezajima.
 CREATE INDEX ind_zpus ON Faktura (ID_fak, zpus_plat);
+-- Pro objednavku neni potreba tvorit index, protoze Objednavka obsahuje pouze 2 polozky a obe bychom
+-- v indexu vyuzili a tedy by nedoslo k zadnemu zrychleni.
 
+-- Vypsani planu po zrychleni pomoci indexu
 EXPLAIN PLAN FOR
 	SELECT zpus_plat, sum(cena)
 	FROM Objednavka INNER JOIN Faktura ON ID_obj = ID_fak
 	GROUP BY zpus_plat;
 SELECT *
 FROM TABLE(DBMS_XPLAN.DISPLAY);
+-- Zde si muzeme vsimnout, ze se opet projde 22 radku, nicmene index ma o jeden sloupec mene nez tabulka,
+-- cena na CPU se snizi na polovinu, tedy na cenu 2.
+-- Zaver tedy je, ze indexovani opravdu dokaze usetrit prostredky, vice by se to ale prokazalo na vetsim mnozstvi dat.
 
 -- KOMPLEXNI SELECT
 -- Dotaz ukazuje, kolik se objednalo ktereho typu zbozi za urcitou cenu, pricemz musel
@@ -591,6 +642,11 @@ GROUP BY typ,
             THEN 'Za 50000 Kc a vice'
     END;
 
+-- Vytvorili jsme materializovany pohled, ktery spojuje tabulky Osoba a Zames, nicmene v nich nebudou obsazeny
+-- kontaktni udaje, rodne cislo a plat.
+-- Muzeme predstavit, ze prijde nejaka kontrola a chtela by nahlednout na to, jake ma obchod zamestance,
+-- ale nechceme zverejnovat jejich osobni informace.
+-- Nebo se takto mohou napriklad vyhledavat zamestanci mezi sebou.
 CREATE MATERIALIZED VIEW PREHLED_ZAM
 BUILD IMMEDIATE
 REFRESH
@@ -622,27 +678,20 @@ GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, ALTER ON PREHLED_ZAM TO xmorku
 -- REVOKE SELECT, INSERT, UPDATE, DELETE, REFERENCES, ALTER ON Nepovolena_objednavka_log FROM xmorku03;
 -- REVOKE SELECT, INSERT, UPDATE, DELETE, REFERENCES, ALTER ON PREHLED_ZAM FROM xmorku03;
 
--- REVOKE ALL ON Osoba FROM xmorku03;
--- REVOKE ALL ON Zakaznik FROM xmorku03;
--- REVOKE ALL ON Zames FROM xmorku03;
--- REVOKE ALL ON Pokladna FROM xmorku03;
--- REVOKE ALL ON Zbozi FROM xmorku03;
--- REVOKE ALL ON Objednavka FROM xmorku03;
--- REVOKE ALL ON Faktura FROM xmorku03;
--- REVOKE ALL ON malo_kontaktu_log FROM xmorku03;
--- REVOKE ALL ON Nepovolena_objednavka_log FROM xmorku03;
--- REVOKE ALL ON PREHLED_ZAM FROM xmorku03;
-
 -- select * from TABLE_PRIVILEGES where GRANTEE = 'XMORKU03';
 
+-- Vybere jmeno, prijmeni a pozici zamestnancu
+-- a seradi je podle pozice a potom podle prijmeni.
 SELECT JMENO, PRIJMENI, POZICE
 FROM PREHLED_ZAM
 ORDER BY POZICE, PRIJMENI;
 
+-- Spocita, kolik zamestanci pracuje na jaky pracovni pomer.
 SELECT PRAC_POM, count(PRAC_POM)
 FROM PREHLED_ZAM
 GROUP BY PRAC_POM;
 
+-- Vybere jmeno a prijmenu vsech pokladnich, ktere pracuji na docasny pracovni pomer.
 SELECT JMENO, PRIJMENI
 FROM PREHLED_ZAM
 WHERE POZICE = 'pokladni' AND PRAC_POM = 'DPP';
